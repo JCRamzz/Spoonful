55c9618b002d2fee5770616b2a1e628fcbb23231
jcramz <jcramz1992@gmail.com>
2023-06-23 18:59:06 -0500

    set up api key and got a functional search

--- spoonful/src/App.jsx
+++ spoonful/src/App.jsx
@@ -1,16 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
+import { API_KEY, APP_ID } from '../env';
+
+
+//const API_KEY = import.meta.env.VITE_API_KEY;
+//const APP_ID = import.meta.env.VITE_APP_ID;
 
 const App = () => {
   const [recipes, setRecipes] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  
 
   useEffect(() => {
     // Function to fetch recipes from the API based on search query
     const fetchRecipes = async () => {
-      const response = await fetch(`API_URL?q=${searchQuery}`);
-      const data = await response.json();
-      setRecipes(data.results);
+      if (searchQuery.trim() === '') {
+        // Don't make the API call if the search query is empty
+        setRecipes([]);
+        return;
+      }
+      
+      try {
+        const response = await fetch(
+        `https://api.edamam.com/search?q=${searchQuery}&app_id=${APP_ID}&app_key=${API_KEY}`
+        );
+        const data = await response.json();
+        console.log(response);
+        setRecipes(data.hits.map((hit) => hit.recipe)); 
+      } catch (error) {
+        console.error('Error fetching recipes:', error);
+        setRecipes([]); // Set recipes to an empty array in case of an error
+      }
+      console.log()
     };
 
     // Call the fetchRecipes function when searchQuery changes
@@ -33,12 +54,16 @@ const App = () => {
       />
       {/* Display the recipe results */}
       <div className="recipe-list">
-        {recipes.map((recipe) => (
-          <div key={recipe.id} className="recipe-card">
-            <img src={recipe.image} alt={recipe.title} />
-            <h3>{recipe.title}</h3>
-          </div>
-        ))}
+      {recipes.length > 0 ? (
+          recipes.map((recipe) => (
+            <div key={recipe.uri} className="recipe-card">
+              <img src={recipe.image} alt={recipe.strMeal} />
+              <h3>{recipe.label}</h3>
+            </div>
+          ))
+        ) : (
+          <p>No recipes found.</p>
+        )}
       </div>
     </div>
   );
